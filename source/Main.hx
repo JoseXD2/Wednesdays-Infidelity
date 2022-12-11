cc55639d2cdff643c9ac9a0a171578ea8ebee26d
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-11 16:50:01 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -30,10 +30,6 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
-	public static var canToggleFullScreen:Bool = false; // Will be set true in Init to make sure everything is ready
-
-	public static var fullscreenKeys:Array<Null<FlxKey>>;
-
 	public static var fpsVar:FPS;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
@@ -66,46 +62,6 @@ class Main extends Sprite
 
 		setupGame();
 
-		addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, function(error:Dynamic)
-		{
-			@:using(haxe.CallStack)
-			var callStack:Array<Dynamic> = CallStack.callStack();
-
-			for (call in callStack)
-			{
-				trace(call);
-			}
-
-			trace(error.message);
-		});
-
-		addEventListener(Event.ENTER_FRAME, update);
-	}
-
-	public function update(e:Event)
-	{
-		if (FlxG.keys == null)
-			return;
-
-		if (canToggleFullScreen && fullscreenKeys != null)
-		{
-			var lastPressed:FlxKey = FlxG.keys.firstJustPressed();
-
-			if (!fullscreenKeys.contains(lastPressed))
-				return;
-
-			for (key in fullscreenKeys)
-			{
-				if (key == null || key == FlxKey.NONE)
-					continue;
-
-				if (key == lastPressed)
-				{
-					FlxG.fullscreen = !FlxG.fullscreen;
-					break;
-				}
-			}
-		}
 	}
 
 	private function setupGame():Void
@@ -128,7 +84,7 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, Init, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		
 		fpsVar = new FPS(10, 5, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -137,7 +93,7 @@ class Main extends Sprite
 		{
 			fpsVar.visible = false;
 		}
-		#end
+		
 
 		#if html5
 		FlxG.autoPause = false;
