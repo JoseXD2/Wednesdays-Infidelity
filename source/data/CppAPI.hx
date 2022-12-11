83bc0e8d87626ae57a65b17029df01c493b8631c
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-11 16:06:53 -0300

    Update CppAPI.hx

--- source/data/CppAPI.hx
+++ source/data/CppAPI.hx
@@ -2,30 +2,39 @@ package data;
 
 class CppAPI
 {
-	#if !android
 	public static function obtainRAM():Int
 	{
+		#if windows
 		return WindowsData.obtainRAM();
+		#end
 	}
-
+       
 	public static function darkMode()
 	{
+		#if windows
 		WindowsData.setWindowColorMode(DARK);
+		#end
 	}
 
 	public static function lightMode()
 	{
+		#if windows
 		WindowsData.setWindowColorMode(LIGHT);
+		#end
 	}
 
 	public static function setWindowOppacity(a:Float)
 	{
+		#if windows
 		WindowsData.setWindowAlpha(a);
+		#end
 	}
 
 	public static function _setWindowLayered()
 	{
+		#if windows
 		WindowsData._setWindowLayered();
+		#end
 	}
-	#end
+
 }
